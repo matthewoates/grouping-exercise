ef6bbf4bb6fd16b37465f04c3892d87862738c5d
Matt <matt@behemoth.(none)>
2013-11-03 19:55:00 -0800

    sanitizing input

--- src/Entry.java
+++ src/Entry.java
@@ -21,6 +21,24 @@ public class Entry {
         return rawName.substring(0, end + 1);
     }
 
+    private static String sanitizeField(String fieldType, String field) {
+        String result = null;
+
+        if (fieldType.equals(MatchType.samePhone.getFieldName())) {
+            result = sanitizePhone(field);
+        } else {
+            result = field.trim();
+        }
+
+        return result;
+    }
+
+    private static String sanitizePhone(String field) {
+        String digits = field.replaceAll("[^0-9]", "");
+
+        return digits.length() > 10 ? digits.substring(digits.length() - 10, digits.length()) : digits;
+    }
+
     Entry(String[] header, String[] fields) {
         assert(header.length == fields.length);
 
@@ -34,7 +52,7 @@ public class Entry {
                 data.put(fieldName, new ArrayList<String>());
             }
 
-            data.get(fieldName).add(fields[i]);
+            data.get(fieldName).add(sanitizeField(header[i], fields[i]));
         }
     }
 
--- src/MatchType.java
+++ src/MatchType.java
@@ -1,17 +1,23 @@
 package grouper;
 
 public enum MatchType {
-    sameEmail("same_email"),
-    samePhone("same_phone");
+    sameEmail("same_email", "Email"),
+    samePhone("same_phone", "Phone");
 
     private String flag;
+    private String fieldName;
 
-    MatchType(String flag) {
+    MatchType(String flag, String fieldName) {
         this.flag = flag;
+        this.fieldName = fieldName;
     }
 
     public String getFlag() {
         return flag;
+    }
+
+    public String getFieldName() {
+        return fieldName;
     }
 
     public static MatchType fromString(String flag) {
