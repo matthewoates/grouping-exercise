b7d2b6d34dd8cc1053042d20b8585ee96b01057a
Matt <matt@behemoth.(none)>
2013-11-03 18:59:34 -0800

    entries are being populated

--- src/Entry.java
+++ src/Entry.java
@@ -1,11 +1,42 @@
 package grouper;
 
+import java.util.*;
+
 public class Entry {
-    Entry(String[] header, String[] fields) {
-        System.out.println("parsing entry!");
-        for (int i = 0; i < fields.length; i++) {
-            System.out.print(fields[i] + ", ");
+    private HashMap<String, ArrayList<String>> data;
+
+    private static String getFieldName(String rawName) {
+        // some data types span multiple fields
+        // ie: Name1, Name2
+        // Here, we will chop off the number at the end
+        int end = rawName.length() - 1;
+
+        while (end > 0 && Character.isDigit(rawName.charAt(end))) {
+            end -= 1;
         }
-        System.out.println("");
+
+        assert(end >= 0);
+
+        return rawName.substring(0, end + 1);
+    }
+
+    Entry(String[] header, String[] fields) {
+        assert(header.length == fields.length);
+
+        data = new HashMap<String, ArrayList<String>>();
+
+        for (int i = 0; i < header.length; i++) {
+            String fieldName = getFieldName(header[i]);
+
+            if (!data.containsKey(fieldName)) {
+                data.put(fieldName, new ArrayList<String>());
+            }
+
+            data.get(fieldName).add(fields[i]);
+        }
+    }
+
+    public ArrayList<String> getField(String field) {
+        return data.get(field);
     }
 }
